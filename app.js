c4349cc200195fb491721daf271efd02b9c0f7d8
korla <johannes.bengtsson@raysearchlabs.com>
2016-03-23 23:47:27 +0100

    textfn added

--- app.js
+++ app.js
@@ -5,7 +5,33 @@ angular.module('rc', [])
   .controller('ctrl', ($scope) => {
     $scope.options = ['One', 'Two', 'Three'];
     $scope.selected = $scope.options[0];
-    $scope.selectVm = new SelectViewModel($scope.options, $scope.selected, 'rc-select/templates/simple.html');
+    $scope.selectVm = new SelectViewModel(
+      $scope.options,
+      $scope.selected,
+      'rc-select/templates/simple.html'
+    );
+
+    $scope.options2 = [
+      {
+        text: 'One',
+        value: 1
+      },
+      {
+        text: 'Two',
+        value: 2
+      },
+      {
+        text: 'Three',
+        value: 3
+      }
+    ];
+    $scope.selected2 = $scope.options2[0];
+    $scope.selectVm2 = new SelectViewModel(
+      $scope.options2,
+      $scope.selected2,
+      'rc-select/templates/simple.html',
+      option => option.text
+    );
   });
 
   require('./rc-select/rc-select.js');
